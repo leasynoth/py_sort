4ac3cfee505b9567c808a98691d2ad5b3b349ee6
leasynoth <kms.stan@gmail.com>
2019-02-01 16:11:13 +1000

    change coctailSort

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -3,15 +3,18 @@
 # bubble sort
 
 __author__ = 'leasynoth'
-__email__ = 'nordiccatinc@gmail.com'
+__email__ = 'nordiccatsinc@gmail.com'
 __DATE__ = '24.01.2019'
 
+import random as rand
+import time
 
 defaultData = [12, 3123234, 234, 2341, 334344, 14421212, 324345, 4, 12323]
 
 def bubbleSort(data=defaultData, way=0):
     
-
+    startTime = time.time()
+    
     if (way == 0): # in descending order
         
         count = 1
@@ -44,22 +47,33 @@ def bubbleSort(data=defaultData, way=0):
         
         print('The second argument indicates the sort order: 0 - in descending order, 1 - in ascending order.')        
     
+    endTime = time.time()
+    
+    print('Lead time: {:.3f} s.'.format(endTime - startTime))
+    
     return data
         
         
 def main():
     
-    defaultData = [12, 3123234, 234, 2341, 334344, 14421212, 324345, 4, 12323] 
+    # uncomment output for visualization 
     
-    print('Default data: ' + str(defaultData) + '\n')
+    randData = list(range(rand.randint(9000, 9000)))
+    rand.shuffle(randData) 
     
-    descend = bubbleSort(defaultData, 0)
+    #print('Default data: \n' + str(randData) + '\n')
     
-    print('Bubble sorting in descending order: ' + str(descend))
+    descend = bubbleSort(randData, 0)
     
-    ascend = bubbleSort(defaultData, 1)
+    #print('Bubble sorting in descending order: \n' + str(descend))
     
-    print('Bubble sorting in ascending order: ' + str(ascend))
+    rand.shuffle(randData)
+    
+    #print('Default data: \n' + str(randData) + '\n')
+    
+    ascend = bubbleSort(randData, 1)
+    
+    #print('Bubble sorting in ascending order: \n' + str(ascend))
     
 
         
--- sorts/coctail_sort.py
+++ sorts/coctail_sort.py
@@ -1,45 +1,81 @@
-# -*-coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 # coctail sort
 
 __author__ = 'leasynoth'
-__email__ = 'nordiccatinc@gmail.com'
+__email__ = 'nordiccastinc@gmail.com'
 __DATE__ = '29.01.2019'
+
+import random as rand
+import time
 
 #defaultData = [232, 57, 3, 36, 123, 2356, 0, 123, 4595, 12356, 45, 985]
 defaultData = [12, 3123234, 234, 2341, 334344, 14421212, 324345, 4, 12323] 
 
 def coctailSort(data=defaultData, way=0): 
     
+    startTime = time.time()
+    
     count = 0
     
     iMin = count
     iMax = len(data)-1
     
-    while (iMin <= iMax):
+    if (way == 0):
         
-        if (count % 2 == 0):
+        while (iMin <= iMax):
             
-            for i in range(iMin, iMax):
+            if (count % 2 == 0):
                 
-                if data[i] > data[i+1]:
+                for i in range(iMin, iMax):
                     
-                    data[i], data[i+1] = data[i+1], data[i]
-                    
-            iMax -= 1
-            
-        elif (count % 2 != 0):
-            
-            for i in range(iMax, iMin, -1):
+                    if data[i] < data[i+1]:
+                        
+                        data[i], data[i+1] = data[i+1], data[i]
+                        
+                iMax -= 1
                 
-                if data[i] < data[i-1]:
+            elif (count % 2 != 0):
+                
+                for i in range(iMax, iMin, -1):
                     
-                    data[i], data[i-1] = data[i-1], data[i]
+                    if data[i] > data[i-1]:
+                        
+                        data[i], data[i-1] = data[i-1], data[i]
+                
+                iMin += 1
             
-            iMin += 1
+            count += 1
+            
+    elif(way == 1):
         
-        count += 1
+         while(iMin <= iMax):
+             
+            if (count % 2 == 0):
+                 
+                for i in range(iMin, iMax):
+                    
+                    if (data[i] > data[i+1]):
+                        
+                        data[i], data[i+1] = data[i+1], data[i]
+                        
+                iMax -= 1         
+                 
+            elif (count % 2 != 0):
+                
+                for i in range(iMax, iMin, -1):
+                    
+                    if (data[i] < data[i-1]):
+                        
+                        data[i], data[i-1] = data[i-1], data[i]
+                        
+                iMin += 1            
         
+            count += 1
+            
+    endTime = time.time()
+    
+    print('Lead time: {:.3f} s.'.format(endTime - startTime))
         
     return data
     
@@ -47,15 +83,24 @@ def coctailSort(data=defaultData, way=0):
     
 def main():
     
-    #defaultData = [232, 57, 3, 36, 123, 2356, 0, 123, 4595, 12356, 45, 985]
+    # uncomment output for visualization 
     
-    defaultData = [12, 3123234, 234, 2341, 334344, 14421212, 324345, 4, 12323] 
+    randData = list(range(rand.randint(9000, 9000)))
+    rand.shuffle(randData) 
     
-    print('Default data: ' + str(defaultData) + '\n')
+    print('Default data: \n' + str(randData) + '\n')
     
-    test = coctailSort(defaultData, 0)
+    descend = coctailSort(randData, 0)
     
-    print('Coctail sorting: ' + str(test))
+    print('Coctail sorting in descending order: \n' + str(descend))
+    
+    rand.shuffle(randData)
+    
+    print('Default data: \n' + str(randData) + '\n')
+    
+    ascend = coctailSort(randData, 1)
+    
+    print('Coctail sorting in ascending order: \n' + str(ascend))
     
     
 if __name__ == '__main__':
